0c1037f0ea3b7729ca400e5ff53ba7ba41ba7d53
MelearningN <uditbh88@gmail.com>
2021-02-08 13:43:42 -0800

    addressing comments

--- app.js
+++ app.js
@@ -14,18 +14,20 @@ app.use('/', indexRouter);
 
 // Page not found middleware
 app.use((req, res, next) => {
-    const err= new Error();
-    err.status=404;
-    next(err)
-  });
+    console.log('404 error handler called');
+    res.status(404).render('page-not-found');
+});
 
 // Error middleware
 app.use((err, req, res, next) => {
-  if (err.status === 404) {
-    res.status(400).render('error', { err, message:'404-Page Not Found' });
-  } else {
-    res.status(err.status || 500).render('error', { err, message:'500-Something is seriously wrong' });
-  }
+    if (err.status === 404) {
+        console.log(err.status, err.message)
+        res.status(400).render('page-not-found', {err});
+    } else {
+        err.message = err.message || "Something is seriously wrong"
+        console.log(err.status, err.message)
+        res.status(err.status || 500).render('error', {err});
+    }
 });
 
 app.listen(3000);
--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const { projects } = require('../data/data.json');
-const {myInfo}= require('../data/data.json');
+const {projects} = require('../data/data.json');
+const {myInfo} = require('../data/data.json');
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {projects});
+router.get('/', function (req, res, next) {
+    res.render('index', {projects});
 });
 
 /* GET about page. */
-router.get('/about', function(req, res, next) {
-  res.render('about', {myInfo});
+router.get('/about', function (req, res, next) {
+    res.render('about', {myInfo});
 });
 
 
 /* GET projects detail page. */
-router.get('/projects/:id', function(req, res, next) {
-  const projectId = req.params.id;
-  const project = projects.find( ({ id }) => id === +projectId );
- 
-   if (project) {
-    res.render('project', { project });
-  } else {
-    res.status(404).render('error', {message:'No such project exist'});
-  }
-}); 
+router.get('/projects/:id', function (req, res, next) {
+    const projectId = req.params.id;
+    const project = projects.find(({id}) => id === + projectId);
+
+    if (project) {
+        res.render('project', {project});
+    } else {
+        const err = new Error();
+        err.status = 404;
+        err.message = "No such project exist"
+        next(err);
+    }
+});
 
 module.exports = router;
